c87d476f61bd27ef543371699abc8f1921d57eae
Hossein <hossein@aleph-labs.com>
2026-04-27 14:57:45 +0800

    feat: replace initial app content with HomePage component and create HomePage with multiple sections

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,25 @@
+import { Navbar } from "@/components/organisms/Navbar";
+import { HeroSection } from "@/components/organisms/HeroSection";
+import { HowItWorksSection } from "@/components/organisms/HowItWorksSection";
+import { PricingSection } from "@/components/organisms/PricingSection";
+import { PastSalesSection } from "@/components/organisms/PastSalesSection";
+import { CharitiesSection } from "@/components/organisms/CharitiesSection";
+import { CharityCtaSection } from "@/components/organisms/CharityCtaSection";
+import { Footer } from "@/components/organisms/Footer";
+
+export function HomePage() {
+  return (
+    <div className="min-h-screen bg-white flex flex-col">
+      <Navbar />
+      <main className="flex-1">
+        <HeroSection />
+        <HowItWorksSection />
+        <PricingSection />
+        <PastSalesSection />
+        <CharitiesSection />
+        <CharityCtaSection />
+      </main>
+      <Footer />
+    </div>
+  );
+}
