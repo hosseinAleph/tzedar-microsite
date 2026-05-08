5546c6def74797894472960fb193685340bc71c6
Hossein <hossein@aleph-labs.com>
2026-05-08 14:58:13 +0800

    chore: update homepage URL in package.json and adjust base path in vite.config.ts for consistency

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/tzedar-microsite/",
+  base: "/tzedar-microsite",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
